48e96c48693b9f3af2b4cb8f0d98725168ab8125
EEC14 <61870511+EEC14@users.noreply.github.com>
2024-12-11 17:50:23 +0100

    Update AuthPage.tsx

--- src/components/AuthPage.tsx
+++ src/components/AuthPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Stethoscope, Mail, Lock, AlertCircle, KeyRound } from 'lucide-react';
 import { createUser, signIn } from '../services/firebase';
 
